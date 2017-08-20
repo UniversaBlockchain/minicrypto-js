9694286eb3c804ea2c96a15d64afd9279f39ab7b
Stanislav Morozov <r3bfish@gmail.com>
2017-08-20 14:25:44 +0300

    Add babel to pack boss protocol

--- webpack.universa.js
+++ webpack.universa.js
@@ -3,5 +3,19 @@ module.exports = [{
   output: {
     filename: 'universa.js',
     path: __dirname + '/build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
   }
 }];
