a98906ec2b6b6fd56fae4cd45055b678cd31f02d
Stanislav Morozov <r3bfish@gmail.com>
2017-08-20 14:52:41 +0300

    Use separate file to get boss protocol

--- index.js
+++ index.js
@@ -2,5 +2,3 @@ exports.hash = require('./lib/hash');
 exports.pki = require('./lib/pki');
 exports.utils = require('./lib/utils');
 exports.cipher = require('./lib/cipher');
-
-exports.Boss = require('./lib/boss');
--- webpack.universa.js
+++ webpack.universa.js
@@ -1,21 +1,51 @@
-module.exports = [{
-  entry: ['./index.js'],
-  output: {
-    filename: 'universa.js',
-    path: __dirname + '/build'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['es2015']
+module.exports = [
+  {
+    entry: ['./index.js'],
+    output: {
+      filename: 'universa.js',
+      path: __dirname + '/build'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015']
+            }
           }
         }
-      }
-    ]
+      ]
+    }
+  },
+  {
+    entry: ['./lib/boss/protocol.js'],
+    output: {
+      filename: 'boss.js',
+      path: __dirname + '/build',
+
+      library: 'Boss',
+      libraryTarget: 'umd'
+    },
+    module: {
+      loaders: [
+        { loader: 'babel-loader' }
+      ],
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015'],
+              plugins: ['transform-es2015-modules-amd']
+            }
+          }
+        }
+      ]
+    }
   }
-}];
+];
