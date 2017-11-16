567057841b3121f4b8202ffd74bef28ccc084a27
anzhu <zhuravlev@asprime.com>
2017-11-16 21:51:12 +0300

    Fix boss object cache

--- test/tools.js
+++ test/tools.js
@@ -3,7 +3,7 @@ const pki = require('../lib/pki');
 const hash = require('../lib/hash');
 const utils = require('../lib/utils');
 
-const { bytesToHex, hexToBytes, arrayToBytes, arrayToBuffer, bytesToArray, randomBytes } = utils;
+const { bytesToHex, hexToBytes, arrayToBytes, arrayToBuffer, bytesToArray, randomBytes, raw } = utils;
 
 describe('Tools', function() {
   it('should convert large binaries', function() {
@@ -14,4 +14,14 @@ describe('Tools', function() {
 
     should(sample).eql(converted);
   });
+
+  it('should convert binaries by parts or not similar', function() {
+    const sample = randomBytes(5000);
+
+    const converted = arrayToBytes(bytesToArray(sample), 13);
+    const converted2 = raw.encode(bytesToArray(sample));
+
+    should(sample).eql(converted);
+    should(sample).eql(converted2);
+  });  
 });
