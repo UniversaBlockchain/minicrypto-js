c306f80893e902760144f251bd9e8346a378ac99
anzhu <zhuravlev@asprime.com>
2018-04-10 00:14:06 +0300

    Add gost test

--- test/tools.js
+++ test/tools.js
@@ -2,6 +2,7 @@ const should = require('should');
 const pki = require('../lib/pki');
 const hash = require('../lib/hash');
 const utils = require('../lib/utils');
+const GOST = require('../lib/hash/gost');
 
 const { bytesToHex, hexToBytes, arrayToBytes, arrayToBuffer, bytesToArray, randomBytes, raw } = utils;
 
@@ -24,4 +25,10 @@ describe('Tools', function() {
     should(sample).eql(converted);
     should(sample).eql(converted2);
   });  
+
+  it.only('should calculate hashId', function() {
+    const gost = new GOST({ name: 'GOST R 34.11', version: 2012 });
+    console.log(gost.digest("abc"));
+    should(1).eql(0);
+  });
 });
